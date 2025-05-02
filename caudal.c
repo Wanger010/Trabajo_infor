5f72298ac5f31903019c147692b7c0004c8955c0
rausiito <raulribagordag5@gmail.com>
2025-05-02 13:22:26 +0200

    Create caudal.c
    Esta será la libreria que utilizaremos para introducir las funciones de nuestro programa

---
+++ caudal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "caudal.h"
+
+//Función para mostrar cuencas y embalses (principal)
+void mostrarCuencasYEmbalses(Embalse* embalses, int totalEmbalses) {
+
+}
+
+// Función 1: Calcular la media anual de caudal para una cuenca específica
+void calcularMediaAnualPorCuenca(Embalse *embalses, int totalEmbalses) {
+
+}
+
+// Función 2: Mostrar la evolución del caudal de la cuenca entre un rango de años
+void evolucionCuenca(Embalse* embalses, int totalEmbalses) {
+
+    }
+
+// Función 3: Comparar el caudal de dos cuencas en un año específico
+void compararCuencas(Embalse* embalses, int totalEmbalses) {
+
+}
+
+// Función 4: Comparar el caudal con la producción agrícola (pendiente de implementación)
+void compararCaudalAgricola(Embalse* embalses, int totalEmbalses) {
+
+}
