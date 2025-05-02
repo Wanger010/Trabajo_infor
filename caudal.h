712f685e624a5c0cd25db1a61483bfe29ff70e12
rausiito <raulribagordag5@gmail.com>
2025-05-02 13:44:37 +0200

    Update caudal.h
    Cambio en la estructura DatosEmbalses para que se recojan los datos del caudal por meses en vez de por años.

--- caudal.h
+++ caudal.h
@@ -4,15 +4,18 @@
 #define CAUDAL_H   // Entonces lo definimos para que no se vuelva a incluir este archivo
 
 #define NUM_ANIOS 10  // Número de años de datos (2012-2021)
+#define NUM_MESES 12  // Número de meses de datos (Enero-Diciembre)
 
 // Estructuras para almacenar la información de los embalses y caudales
+// Estructura que almacena datos del caudal de los embalses en el rango de años
+// dado y por meses.
 typedef struct {
     // Array de enteros que guarda los años correspondientes a los datos de caudal
     // Por ejemplo: [2012, 2013, 2014, ..., 2021]
     int anios[NUM_ANIOS];
-    // Array de números decimales (float) que guarda los caudales en Hm³ para cada año
+    // Array de números decimales (float) que guarda los caudales en Hm³ para cada mes
     // Por ejemplo: [54.2, 60.1, 48.7, ..., 70.5]
-    float caudales[NUM_ANIOS];
+    float caudales[NUM_MESES];
 } DatosEmbalse;
 
 // Definición de una estructura llamada 'Embalse' que agrupa información sobre un embalse
@@ -22,7 +25,7 @@ typedef struct {
     // Nombre del embalse (ej: "Alcantara", "La Serena", etc.)
     char embalseNombre[100];
     // Mes al que corresponde la medición (ej: "enero", "febrero")
-    char mes[20];
+    char mes[NUM_MESES];
     // Estructura que contiene los datos anuales de caudal para este embalse y mes
     DatosEmbalse datos;
 } Embalse;
